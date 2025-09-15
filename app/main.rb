7c43cd5405fb931eddfed425631a73cb8f2a7c0e
Kehvarl <Kehvarl@Kehvarl.com>
2025-09-14 18:06:08 -0700

    bigger, better, more vertical

--- app/main.rb
+++ app/main.rb
@@ -1,33 +1,23 @@
 def init args
-  args.state.falling = []
-  args.state.landed = []
-  args.state.heights = Array.new(1280,0)
+  args.state.blocks = []
 end
 
 def calc_physics args
-  args.state.falling.each do |b|
+  args.state.blocks.each do |b|
     b.y -= b.vy
     b.x += b.vx
-    if b.x < 0 or b.x > 1280
-      b.x = b.x.clamp(0,1280)
+    if b.x < 0 or b.x > 720
+      b.x = b.x.clamp(0,720)
       b.vx = -b.vx
     end
-    if args.state.heights[b.x...b.x+b.w].any?{|h| b.y <= h}
-      b.vy = 0
-      args.state.landed << b
-      args.state.heights[b.x...b.x+b.w].each_with_index do |h,i|
-        args.state.heights[i+b.x] += b.h
-      end
-    end
   end
-  args.state.falling.reject!{|f| f.vy <= 0}
 end
 
 def add_block args
-  args.state.falling << {
-    x: rand(1280), y: rand(50) + 670, w: 8, h: 8,
+  args.state.blocks << {
+    x: rand(18)*40, y: (rand(2) + 30)*40, w: 40, h: 40,
     path: "sprites/circle/blue.png",
-    vy: 1, vx: [-1,-0.5,-0.25,-0.1,0,0.1,0.25,0.5,1].sample()
+    vy: 1, vx: 0
     }
 end
 
@@ -41,16 +31,10 @@ def tick args
     add_block(args)
   end
 
-  if args.inputs.keyboard.c
-    puts args.state.falling.size
-    puts args.state.landed.size
-  end
-
   calc_physics(args)
 
-  args.outputs.primitives << args.state.landed
+  args.outputs.primitives << args.state.blocks
 
-  args.outputs.primitives << args.state.falling
 
 end
 
