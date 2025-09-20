b08c1763869c0cf9515091b4cc0f3f9c3b6fc16c
Kehvarl <Kehvarl@Kehvarl.com>
2025-09-19 21:33:18 -0700

    grid-based

--- app/main.rb
+++ app/main.rb
@@ -1,73 +1,66 @@
 def init args
-  args.state.falling = []
-  args.state.landed = []
-  args.state.dropper = {
-    x: 360, y: 1250, w: 40, h: 20,
-    path: "sprites/square/green.png",
-  }
-  args.state.heights = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-  args.state.drop_counter = 45
-end
-
-def calc_physics args
-  args.state.falling.each do |b|
-    b.y -= b.vy
-    b.x += b.vx
-    if b.x < 0 or b.x > 720
-      b.x = b.x.clamp(0,720)
-      b.vx = -b.vx
-    end
-    if b.y <= args.state.heights[b.x/40]
-      b.y = args.state.heights[b.x/40].clone
-      b.vy = 0
-      b.vx = 0
-      args.state.heights[b.x/40] += 40
-      args.state.landed << b
-    end
-  end
-  args.state.falling = args.state.falling.select{|b| b.vy > 0}
-end
-
-def check_block args, b
-  # Block is different color from block below, else destroy both
-  # All touching blocks in row are same color, else destroy all
-  # Each row is shorter than the below row, else cannot win
-  # Tallest possible:  15
-end
-
-
-def add_block args
-  args.state.falling << {
-    x: args.state.dropper.x, y: 1210, w: 40, h: 40,
-    path: "sprites/circle/blue.png",
-    vy: 10, vx: 0
+    args.state.cats = {}
+    args.state.dropper = {
+        x: 360, y: 1250, w: 40, h: 20,
+        path: "sprites/square/green.png",
     }
 end
 
-def tick args
-  if args.tick_count == 0
-    init(args)
-    add_block(args)
-  end
-  args.state.drop_counter -=1
-
-  if args.inputs.keyboard.key_down.left
-    args.state.dropper.x -= 40
-  elsif args.inputs.keyboard.key_down.right
-    args.state.dropper.x += 40
-  end
-
-  if args.state.drop_counter <= 0 and (args.inputs.keyboard.space or args.inputs.mouse.button_left)
-    add_block(args)
-    args.state.drop_counter = 45
-  end
-
-  args.state.dropper.x = args.state.dropper.x.clamp(0,680)
-
-  calc_physics(args)
-
-  args.outputs.primitives << args.state.dropper
-  args.outputs.primitives << args.state.falling
-  args.outputs.primitives << args.state.landed
+def add_block args
+    x = args.state.dropper.x
+    y = 1210
+    args.state.cats[[x,y]] = {
+        x: x, y: y, w: 40, h: 40,
+        path: "sprites/circle/blue.png",
+        vy: 10, vx: 0
+    }
 end
 
+def calculate_physics args
+    new_grid = {}
+    args.state.cats.each do |c|
+        x,y = c[0]
+        if not args.state.cats.has_key?([x,y-1])
+            y -= 1
+            cat = c[1]
+            cat.y -= 40
+            new_grid[[x,y]] = cat
+        end
+    end
+end
+
+def check_rules args
+    # Landed cat on cat of same color teleports both away
+    # Landed cat adjacent to different color teleports both away
+    #
+end
+
+def process_inputs args
+    if args.inputs.keyboard.right
+        args.state.dropper.x += 40
+    elsif args.inputs.keyboard.left
+        args.state.dropper.x -= 40
+    end
+    if args.inputs.keyboard.space
+        add_block(args)
+    end
+end
+
+def render args
+    args.outputs.primitives << args.state.dropper
+    args.state.cats.each do |c|
+        args.outputs.primitives << c[1]
+    end
+end
+
+def tick args
+    if args.tick_count == 0
+        init(args)
+        add_block(args)
+    end
+    process_inputs(args)
+    calculate_physics(args)
+    check_rules(args)
+
+    render(args)
+end
